11c02ef53f4007c9ec88337d9d665ccba2428302
Frizzus <loiccanin.pro@gmail.com>
2023-04-07 11:36:06 +0200

    ajout de l'interface pauseMenu

---
+++ _app/immutable/chunks/1.77c7a69a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.54365e6a.js";export{t as component};
---
+++ _app/immutable/chunks/2.88e09966.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.0bd9d774.js";export{t as component};
